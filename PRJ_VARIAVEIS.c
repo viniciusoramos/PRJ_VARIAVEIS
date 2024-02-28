20a200db6b3b881dcc81ac6428fe602eb6c63bad
viniciusoramos <161609090+viniciusoramos@users.noreply.github.com>
2024-02-28 12:13:39 -0300

    Add files via upload

---
+++ PRJ_VARIAVEIS.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+int main(void){
+    int idade = 26;
+    const int MAX_NUM = 100;
+    
+    printf("Minha variável int comum = %d\n\n", idade);
+    printf("Minha variável constante = %d\n\n", MAX_NUM);
+    printf("Minha constante = %f\n", PI);
+    
+    MAX_NUM = 99;
+    
+    
+    
+    
+    return EXIT_SUCCESS;
+}
+
